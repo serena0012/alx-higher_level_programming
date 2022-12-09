a99c648b158435183fbaf8a5fa13bd6092d36067
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-12-09 10:38:15 -0800

    Task 15 corrected

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 def complex__delete(a_dictionary, value):
-    for key, val in list(a_dictionary.items()):
-        if val == value:
-            del a_dictionary[key]
-    return a_dictionary
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+             if v == value:
+                 del a_dictionary[k]
+                 break
+
+    return (a_dictionary)
