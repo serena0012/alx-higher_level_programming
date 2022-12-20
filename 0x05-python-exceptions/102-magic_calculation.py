62112e9e5c9695aba4621bb12c4b04b31067505d
Gbenga Elegbede <jonathangnenga@gmail.com>
2022-12-20 23:18:06 +0100

    Task 8 & 9 editted

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-# Gbenga Elegbede
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -10,9 +6,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             else:
-                result += a ** b / i
-        except:
+                result += (a ** b) / i
+        except Exception:
             result = b + a
             break
-    return (result
-            )
+    return (result)
