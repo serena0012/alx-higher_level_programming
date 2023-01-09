4ae89dfc4bca09465c269aa383f272e787efcb9e
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-01-10 00:57:01 +0100

    All tasks completed

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-lookup.py
+
+"""Define an object attribute lookup function."""
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    Return (dir(obj))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
