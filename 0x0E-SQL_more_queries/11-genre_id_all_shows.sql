542b7d93d74595dacdd5689a286daf05adb711b3
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-02-15 21:33:44 -0300

    some files selected

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,6 @@
---Lists all shows contained in the database hbtn_0d_tvshows.
--- Displays NULL for shows without genres.
--- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-       LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT a.title, b.genre_id
+FROM tv_shows a
+LEFT JOIN tv_show_genres b
+ON a.id = b.show_id
+ORDER BY a.title ASC, b.genre_id ASC;
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,7 @@
---Lists all genres from the database hbtn_0d_tvshows along with the number of
--- shows linked to each.
--- Does not display genres without linked shows.
--- Records are ordered by descending number of shows linked.
-SELECT g.`name` AS `genre`,
-       COUNT(*) AS `number_of_shows`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS t
-       ON g.`id` = t.`genre_id`
- GROUP BY g.`name`
- ORDER BY `number_of_shows` DESC;
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT a.name AS genre, COUNT(b.genre_id) AS number_of_shows
+FROM tv_genres a
+JOIN tv_show_genres b
+ON a.id = b.genre_id
+GROUP BY b.genre_id
+ORDER BY number_of_shows DESC;
