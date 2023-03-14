9a94e424b0281689c331758a163e7c4855b245ea
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-03-14 20:42:42 +0100

    Task 5 & 6 editted

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.experts = class square extends require('./4-rectangle.js') {
+module.experts = class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
