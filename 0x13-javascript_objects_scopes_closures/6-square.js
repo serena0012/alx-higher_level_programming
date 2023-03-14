ec198639a705615dd38847734d76ab40ae0f44e7
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-03-14 21:11:19 +0100

    Task 5 & 6 updated

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-const Square5 = require('./5-square');
-
-class Square extends Square5 {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    if (typeof (c) === 'undefined') {
-      c = 'X';
-    }
-
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
-
-module.exports = Square;
+};
