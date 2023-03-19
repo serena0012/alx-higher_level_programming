f83840cac7418aa543d3805e6a3311d5ed26a4e5
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-03-19 06:59:38 +0100

    Task 4 upddated

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-This script lists all cities
-from the database `hbtn_0e_4_usa`
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -9,26 +11,18 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the cities
+    Access to the database and get the states
     from the database.
     """
 
-db = MySQLdb.connect(host='localhost', port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-cur.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
-        """)
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-cities = cur.fetchall()
-for city in cities:
-    print(city)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
