14d92851858b1f09246cd07cbb98dd1c1ca6c53e
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-04-25 21:15:14 +0100

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
-#!/usr/bin node
+#!/usr/bin/node
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (error, content) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log(content);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
