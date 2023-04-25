76ab25e5e657c3c702ac9e2ca1a6f4b4bb3fb3ae
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-04-25 20:59:55 +0100

    Task 0 completed

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(content);
+});
+
